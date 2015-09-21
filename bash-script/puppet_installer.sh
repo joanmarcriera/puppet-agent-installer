49b0907e1d2521835c7ddb2d6cb4c4c96ecc0a7d
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2015-09-21 11:47:51 +0100

    debian suport added

--- bash-script/puppet_installer.sh
+++ bash-script/puppet_installer.sh
@@ -186,25 +186,30 @@ setUpInstallPkg()
 	  echo -n "Installing $i on    $DistroBasedOn      ."
                 case $DistroBasedOn in
                         Debian | debian )
-				case ${REVISION} in 
-				6)
-					DEBIANRELEASE=squeeze
-					die $DEBIANRELEASE
-					;;	
-				7)
-					DEBIANRELEASE=wheezy
-					die $DEBIANRELEASE
-					;;	
-				8)
-					DEBIANRELEASE=jessie
-					;;	
-				*)
-					;;	
-				esac
-				DEBIANPACKAGE=puppetlabs-release-${DEBIANRELEASE}.deb
-				wget https://apt.puppetlabs.com/$DEBIANPACKAGE
-				dpkg -i ${DEBIANPACKAGE}
-				apt-get update
+				# Older versions of Debian don't have lsb_release by default, so 
+				# install that if we have to.
+				which lsb_release || apt-get install -y lsb-release
+				
+				# Load up the release information
+				DISTRIB_CODENAME=$(lsb_release -c -s)
+				
+				REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb"
+				
+				apt-get install -y wget > /dev/null
+				apt-get update > /dev/null
+
+				# Install the PuppetLabs repo
+				echo "Configuring PuppetLabs repo..."
+				repo_deb_path=$(mktemp)
+				wget --output-document="${repo_deb_path}" "${REPO_DEB_URL}" 2>/dev/null
+				dpkg -i "${repo_deb_path}" >/dev/null
+				apt-get update >/dev/null
+
+				# Install Puppet
+				echo "Installing Puppet..."
+				DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install puppet >/dev/null
+
+				echo "Puppet installed!"
 
                                 ;;
                         SuSe | suse )
@@ -268,6 +273,11 @@ return 0
 
 [ $(is_root) ] && die "Must be root." || tell "Root => ok." 
 [ `ps axu|grep puppet |grep -v grep|grep -v $0 | wc -l` -gt 0 ] && die "puppet already running"
+#is puppet already installed
+if which puppet > /dev/null 2>&1; then
+	echo "Puppet is already installed."
+	exit 0
+fi
 
 [ $debug -eq 1 ] && shootProfile debug || shootProfile 
 setUpInstallPkg
